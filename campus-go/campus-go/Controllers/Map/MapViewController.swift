de4d796cfac35dbf1abdebae810d9a9473e213fb
Murilo Gonçalves <murilogoncalves127@gmail.com>
2021-10-04 09:50:09 -0300

    (feature) map starts zoomed in Unicamp

--- campus-go/campus-go/Controllers/Map/MapViewController.swift
+++ campus-go/campus-go/Controllers/Map/MapViewController.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let initialLocation = MapConstants.unicamp
+        mapView.centerToLocation(initialLocation)
+    }
+}
+
+private extension MKMapView {
+    func centerToLocation(
+        _ location: CLLocation,
+        regionRadius: CLLocationDistance = 1500
+    ) {
+        let coordinateRegion = MKCoordinateRegion(
+            center: location.coordinate,
+            latitudinalMeters: regionRadius,
+            longitudinalMeters: regionRadius)
+        
+        setRegion(coordinateRegion, animated: true)
+    }
+}
---
+++ campus-go/campus-go/Models/Map/MapConstants.swift
@@ -0,0 +1,13 @@
+//
+//  MapConstants.swift
+//  campus-go
+//
+//  Created by Murilo Gonçalves on 04/10/21.
+//
+
+import Foundation
+import CoreLocation
+
+struct MapConstants {
+    static let unicamp = CLLocation(latitude: -22.8184, longitude:  -47.0647)
+}
