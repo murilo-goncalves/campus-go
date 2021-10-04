7bc2e63c55c03ebecf55c911028ac01678d5a935
Vitor Moriya <vitorjmoriya@gmail.com>
2021-10-04 10:34:15 -0300

    CollectionView com todos os achievements

---
+++ campus-go/campus-go/Controllers/Achievement/AchievementListController.swift
@@ -0,0 +1,41 @@
+//
+//  AchievementListController.swift
+//  campus-go
+//
+//  Created by Vitor Jundi Moriya on 04/10/21.
+//
+
+import Foundation
+import UIKit
+
+class AchievementListController: UIViewController {
+    
+    @IBOutlet var achievementListView: AchievementList!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        achievementListView.achievementCollection.delegate = self
+        achievementListView.achievementCollection.dataSource = self
+    }
+}
+
+extension AchievementListController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cellView = achievementListView.achievementCollection.dequeueReusableCell(withReuseIdentifier: "achievementCell", for: indexPath as IndexPath) as! AchievementCell
+        cellView.titleLabel.text = "Lorem Ipsum"
+        cellView.image.image = UIImage(named: "books")
+        cellView.layer.borderColor = UIColor(rgb: 0xC7C7CC).cgColor
+        cellView.layer.borderWidth = 0.5
+        return cellView
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+        print("You selected cell #\(indexPath.item)!")
+
+    }
+}
