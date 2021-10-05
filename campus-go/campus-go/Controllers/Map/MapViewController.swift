037d1faffb08f9ce5c5b27a16a484530661d085c
Murilo Gonçalves <murilogoncalves127@gmail.com>
2021-10-05 10:57:22 -0300

    (feature) search controller for places and table view showing search results

--- campus-go/campus-go/Controllers/Map/MapViewController.swift
+++ campus-go/campus-go/Controllers/Map/MapViewController.swift
@@ -9,27 +9,6 @@ import Foundation
 import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController, CLLocationManagerDelegate {
-    
-    @IBOutlet weak var mapView: MKMapView!
-    var locationManager: CLLocationManager!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupLocationManager()
-        
-        let initialLocation = MapConstants.unicamp
-        mapView.centerToLocation(initialLocation)
-        
-    }
-    
-    func setupLocationManager() {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        locationManager = appDelegate.locationManager
-        locationManager.delegate = self
-    }
-}
-
 private extension MKMapView {
     func centerToLocation(
         _ location: CLLocation,
@@ -41,5 +20,99 @@ private extension MKMapView {
             longitudinalMeters: regionRadius)
         
         setRegion(coordinateRegion, animated: true)
+    }
+}
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var topView: UIView!
+    
+    private var searchController: UISearchController!
+    private var resultsTableViewController: ResultsTableViewController!
+    
+    private let searchCompleter = MKLocalSearchCompleter()
+    private var searchResults = [MKLocalSearchCompletion]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupMapView()
+        setupResultsTableView()
+        setupSearchController()
+        
+        searchCompleter.delegate = self
+    }
+    
+    private func setupMapView() {
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        let initialLocation = MapConstants.unicamp
+        mapView.centerToLocation(initialLocation)
+    }
+    
+    private func setupResultsTableView() {
+        resultsTableViewController = self.storyboard?.instantiateViewController(withIdentifier: "ResultsTableViewController") as? ResultsTableViewController
+        resultsTableViewController?.tableView.delegate = self
+        resultsTableViewController?.tableView.dataSource = self
+    }
+    
+    private func setupSearchController() {
+        searchController = UISearchController(searchResultsController: resultsTableViewController)
+        searchController.searchResultsUpdater = resultsTableViewController
+        searchController.searchBar.delegate = self
+        topView.addSubview(self.searchController.searchBar)
+        searchController.searchBar.sizeToFit()
+        searchController.searchBar.frame.size.width = topView.frame.size.width
+        searchController.searchBar.searchBarStyle = .minimal
+    }
+}
+
+// MARK: Extensions
+
+extension MapViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        print(indexPath.row)
+    }
+}
+
+extension MapViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let searchResult = searchResults[indexPath.row]
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        cell.textLabel?.text = searchResult.title
+        cell.detailTextLabel?.text = searchResult.subtitle
+        
+        return cell
+    }
+}
+
+extension MapViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchCompleter.queryFragment = searchText
+    }
+}
+
+extension MapViewController: MKLocalSearchCompleterDelegate {
+    
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        searchResults = completer.results
+        resultsTableViewController.tableView.reloadData()
+    }
+    
+    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
+        // handle error
     }
 }
---
+++ campus-go/campus-go/Controllers/Map/ResultsTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ResultsTableViewController.swift
+//  campus-go
+//
+//  Created by Murilo Gonçalves on 05/10/21.
+//
+
+import Foundation
+import UIKit
+
+class ResultsTableViewController: UITableViewController {
+    
+}
+
+extension ResultsTableViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        
+    }
+}
