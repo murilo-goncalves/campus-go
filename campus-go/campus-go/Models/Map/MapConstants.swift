92af16ecb101b3302775d9cd662b1bba428b1994
Murilo Gonçalves <murilogoncalves127@gmail.com>
2021-11-11 11:32:42 -0300

    (refactor) moved magic numbers from map to MapConstants

--- campus-go/campus-go/Models/Map/MapConstants.swift
+++ campus-go/campus-go/Models/Map/MapConstants.swift
@@ -11,7 +11,18 @@ import CoreLocation
 struct MapConstants {
     
     static let unicamp = CLLocation(latitude: -22.8184, longitude:  -47.0647)
+    static let unicampCoordinate = CLLocationCoordinate2D(latitude: -22.8184, longitude:  -47.0647)
+    static let cbCoordinate =  CLLocationCoordinate2D(latitude: -22.817029, longitude:  -47.069759)
+    static let pracaPazCoordinate = CLLocationCoordinate2D(latitude: -22.822403, longitude:  -47.067731)
+    
     static let initialRegionRadius: CLLocationDistance = 1500
+    
     static let resultsTableViewNumberOfSections = 1
     
+    static let routeLineWidth = 5.0
+    
+    static let annotationHeight = 30
+    static let annotationWidth = 18
+    static let selectedAnnotationHeight = 60
+    static let selectedAnnitationWidht = 36
 }
