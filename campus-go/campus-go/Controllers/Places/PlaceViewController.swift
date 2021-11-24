ee3c44211562779a426ba2ade991b614d8434116
Giordano Mattiello <giordanomattiello@MacBook-Pro-de-Giordano.local>
2021-11-24 10:35:16 -0300

    Alterando botoes para defalt e usando reloadData na collectionView

--- campus-go/campus-go/Controllers/Places/PlaceViewController.swift
+++ campus-go/campus-go/Controllers/Places/PlaceViewController.swift
@@ -11,6 +11,8 @@ import Foundation
 class PlaceViewController: UIViewController, UIScrollViewDelegate, UICollectionViewDelegate {
     
     @IBOutlet var placeView: PlaceView!
+    @IBOutlet weak var goButton: UIButton!
+    @IBOutlet weak var placeButton: UIButton!
     
     var place = Place()
     var pictureService = Pictures()
@@ -50,23 +52,19 @@ class PlaceViewController: UIViewController, UIScrollViewDelegate, UICollectionV
         placeView.pageControl.currentPage = 0
 
         
+        
         if let userCoord = userCoordinate{
             let dist = calculaDistancia(userCoord, placeCoordinate)
             placeView.distanciaLugar.text = dist < 1.0 ? "\(dist*1000) m" : "\((dist*10).rounded()/10) km"
         } else {
             placeView.distanciaLugar.text = ""
         }
-
-        
         
         placeView.recentAchievement.layer.cornerRadius = 15.0
         placeView.recentAchievement.layer.borderWidth = 5.0
         placeView.recentAchievement.layer.borderColor = UIColor.clear.cgColor
         placeView.recentAchievement.layer.masksToBounds = true
-       
-        //apenas usando a informação do PlacesViewController
-        //print("You tapped me", indexPath! )
-        
+               
         var currentImageView: UIImageView! = nil
         for index in 0..<images.count{
 
@@ -116,40 +114,36 @@ class PlaceViewController: UIViewController, UIScrollViewDelegate, UICollectionV
         placeView.recentAchievement.register(AchievementCollectionViewCell.nib(), forCellWithReuseIdentifier: AchievementCollectionViewCell.identifier)
         let layout = UICollectionViewFlowLayout()
         placeView.recentAchievement.collectionViewLayout = layout
+        roundButtonsCorners()
         
     }
+    private func roundButtonsCorners(){
+        goButton.layer.cornerRadius = 5
+        placeButton.layer.cornerRadius = 5
+    }
     
-    //Depois de aparecer na tela
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         placeView.recentAchievement.layer.borderWidth = 1
         placeView.recentAchievement.layer.borderColor = UIColor.lightGray.cgColor
         placeView.recentAchievement.layer.cornerRadius = 5
-        //print(placeView.scrollView.frame)
     }
     
-    //Antes de aparecer na tela
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        //print(placeView.scrollView.frame)
     }
     
     //Calcula a distância entre o usuário e o lugar
-    
     func calculaDistancia(_ userLocation: CLLocationCoordinate2D?, _ placeLocation: CLLocationCoordinate2D?) -> Double {
         
-        guard let userLocation = userLocation else {
-            return -1.0
-        }
-        guard let placeLocation = placeLocation else {
-            return -1.0
-        }
-        
+        guard let userLocation = userLocation else { return -1.0 }
+        guard let placeLocation = placeLocation else { return -1.0 }
         let uLocation = CLLocation(latitude: userLocation.latitude, longitude: userLocation.longitude)
         let pLocation = CLLocation(latitude: placeLocation.latitude, longitude: placeLocation.longitude)
         var distance  = uLocation.distance(from: pLocation) as Double
         formataDistancia(&distance)
         return distance
+        
     }
     
     func formataDistancia(_ distance: inout Double) {
@@ -169,6 +163,7 @@ class PlaceViewController: UIViewController, UIScrollViewDelegate, UICollectionV
         _ = navigationController?.popViewController(animated: true)
         self.navigationController?.tabBarController?.selectedIndex = 1
     }
+    
 }
 
 
@@ -189,26 +184,27 @@ extension PlaceViewController: UICollectionViewDataSource{
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AchievementCollectionViewCell.identifier, for: indexPath) as! AchievementCollectionViewCell
         cell.configure(hasProgress: false)
         return cell
-        
     }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showAchievement" {
             let destVC = segue.destination as! AchievementController
             destVC.loadViewIfNeeded()
         }
     }
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         self.performSegue(withIdentifier: "showAchievement", sender: collectionView.cellForItem(at: indexPath))
         collectionView.deselectItem(at: indexPath, animated: true)
-        
     }
-
+    
 }
 extension PlaceViewController: UICollectionViewDelegateFlowLayout{
     // MARK: UICollectionViewDelegateFlowLayout methods
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: placeView.recentAchievement.frame.width ,height: 76)
     }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 0
     }
