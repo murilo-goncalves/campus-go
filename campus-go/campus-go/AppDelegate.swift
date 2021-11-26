f6d4a7c5c4f32ed8da7552eceacd3586fe5df17f
Vinicius Flores Ribeiro <vinicius.flores@kascosys.com.br>
2021-11-26 12:09:00 -0300

    arrumando os bugs / task finalizada

--- campus-go/campus-go/AppDelegate.swift
+++ campus-go/campus-go/AppDelegate.swift
@@ -80,6 +80,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             for object in result {
                 let _ = try! PlaceService().create(name: object.name!, latitude: object.latitude, longitude: object.longitude, placeID: object.placeID, nImages: object.nImages)
             }
+            let teste = try PlaceService().readAll()
+            
+            _ = try! UserService().create(name: "placeholder", xp: 420)
+            
         } catch {
             print("\(error)")
         }
