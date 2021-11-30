3853f424f6d9457c6ff1ed0c88c05047e80474ff
Luiz Gustavo Silva Aguiar <l.gustavo092000@gmail.com>
2021-11-30 10:28:59 -0300

    Corrigido últimos detalhes na parte de achievements

--- campus-go/campus-go/Views/Components/AchievementProgressView.swift
+++ campus-go/campus-go/Views/Components/AchievementProgressView.swift
@@ -24,27 +24,35 @@ class AchievementProgressView: UIView {
     
     func commonInit(){
         Bundle.main.loadNibNamed("AchievementProgressView", owner: self, options: nil)
-        let path = UIBezierPath(ovalIn: CGRect(x: contentView.frame.origin.x, y: contentView.frame.origin.y, width: contentView.frame.width, height: contentView.frame.height))
-        let progressLayer = CAShapeLayer()
-        progressLayer.path = path.cgPath
-        progressLayer.fillColor = UIColor.clear.cgColor
-        progressLayer.strokeColor = Color.orangeTrans.cgColor
-        progressLayer.strokeStart = 0
-        progressLayer.strokeEnd = 1
-        progressLayer.lineWidth = 2
-        progressLayer.lineCap = .round
-        contentView.layer.addSublayer(progressLayer)
         addSubview(contentView)
     }
     
     func setProgress(progress:Double) {
+        
         let path = UIBezierPath(arcCenter: CGPoint(x: contentView.frame.origin.x + contentView.frame.width/2, y: contentView.frame.origin.y + contentView.frame.width/2), radius: contentView.frame.width/2, startAngle: CGFloat(-Double.pi/2), endAngle: CGFloat(3 * Double.pi/2), clockwise: true)
         let layer = CAShapeLayer()
         layer.path = path.cgPath
         layer.fillColor = UIColor.clear.cgColor
-        layer.strokeColor = Color.orange.cgColor
         layer.strokeStart = 0
-        layer.strokeEnd = progress
+        layer.strokeEnd = 1
+        if (progress == 0.0) {
+            layer.strokeColor = Color.purpleTrans.cgColor
+        } else if (progress == 1.0) {
+            layer.strokeColor = Color.green.cgColor
+        } else {
+            layer.strokeColor = Color.orange.cgColor
+            layer.strokeEnd = progress
+            let path = UIBezierPath(ovalIn: CGRect(x: contentView.frame.origin.x, y: contentView.frame.origin.y, width: contentView.frame.width, height: contentView.frame.height))
+            let progressLayer = CAShapeLayer()
+            progressLayer.path = path.cgPath
+            progressLayer.fillColor = UIColor.clear.cgColor
+            progressLayer.strokeColor = Color.orangeTrans.cgColor
+            progressLayer.strokeStart = 0
+            progressLayer.strokeEnd = 1
+            progressLayer.lineWidth = 2
+            progressLayer.lineCap = .round
+            contentView.layer.addSublayer(progressLayer)
+        }
         layer.lineWidth = 2
         layer.lineCap = .round
         contentView.layer.addSublayer(layer)
