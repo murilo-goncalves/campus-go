40ad7c6f58e75cdb15f3a7dee6f9d61f7defda75
Luiz Gustavo Silva Aguiar <l.gustavo092000@gmail.com>
2021-12-03 10:37:53 -0300

    fiz nada ainda

---
+++ campus-go/campus-go/Validator.swift
@@ -0,0 +1,16 @@
+//
+//  Validator.swift
+//  campus-go
+//
+//  Created by Luiz Gustavo Silva Aguiar on 03/12/21.
+//
+
+import Foundation
+
+class Validator {
+    let userAttributes = UserAttributes()
+    
+    private validateFirstPlace() {
+        
+    }
+}
