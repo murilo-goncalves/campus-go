8c2c90a873c439d93f42482e097ebd8aa89e2fe5
Luiz Gustavo Silva Aguiar <l.gustavo092000@gmail.com>
2021-12-05 18:40:00 -0300

    Arrumei um detalhe minimo aqui

--- campus-go/campus-go/AppDelegate.swift
+++ campus-go/campus-go/AppDelegate.swift
@@ -185,22 +185,20 @@ extension AppDelegate: CLLocationManagerDelegate {
                 }
             })
             routeDelegate?.didTapCancel()
-            //Se o lugar já havia sido descoberto então não há nada mais para fazer
-            if(wasDiscovered) {
-                return
-            }
-            let alertUtil = AlertUtil()
-            var place: Place?
-            do {
-                place = try placeService.read(uid: uid!)
-            }catch{
-                print(error)
-            }
-            if let currentViewController = getCurrentViewController(){
-                if let alertViewController = currentViewController.children[0] as? AlertViewDelegate {
-                    alertUtil.showAlert(viewController: alertViewController,place: place,achievement: nil)
-                }else{
-                    print("Erro ao encontrar a view")
+            if(!wasDiscovered) {
+                let alertUtil = AlertUtil()
+                var place: Place?
+                do {
+                    place = try placeService.read(uid: uid!)
+                }catch{
+                    print(error)
+                }
+                if let currentViewController = getCurrentViewController(){
+                    if let alertViewController = currentViewController.children[0] as? AlertViewDelegate {
+                        alertUtil.showAlert(viewController: alertViewController,place: place,achievement: nil)
+                    }else{
+                        print("Erro ao encontrar a view")
+                    }
                 }
             }
             
