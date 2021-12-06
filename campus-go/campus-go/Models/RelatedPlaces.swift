8a4edc7df86e737cab3b20215a04c1365cdcfceb
Luiz Gustavo Silva Aguiar <l.gustavo092000@gmail.com>
2021-12-06 16:31:06 -0300

    Criei o modal relatedPlaces que vc da uma conquista e ele te devolve uma lista com os achievementsrelacionados e inseri o campo relatedPlaces no json

--- campus-go/campus-go/Models/RelatedPlaces.swift
+++ campus-go/campus-go/Models/RelatedPlaces.swift
@@ -11,14 +11,32 @@ struct RelatedPlaces {
     var achievementPlaces: [UUID] = []
     init(achievement: Achievement) {
         guard let list = achievement.relatedPlaces?.components(separatedBy: " ") else { return }
-        for id in list {
+        //Não existe lugar relacionado a esta conquista
+        if list[0] == "None" {
+            return
+        }
+        //Todos os lugares estão relacionados a esta conquista
+        else if list[0] == "All" {
             do {
-                let uid_ = try PlaceService().readByID(placeID: Int64(id) ?? 0)
-                if let uid = uid_ {
-                    achievementPlaces.append(uid)
+                guard let places = try PlaceService().readAll() else { return }
+                for place in places {
+                    achievementPlaces.append(place.uid!)
                 }
             } catch {
                 print(error)
+            }
+        }
+        //busca os lugares relacionados
+        else {
+            for id in list {
+                do {
+                    let uid_ = try PlaceService().readByID(placeID: Int64(id) ?? 0)
+                    if let uid = uid_ {
+                        achievementPlaces.append(uid)
+                    }
+                } catch {
+                    print(error)
+                }
             }
         }
     }
